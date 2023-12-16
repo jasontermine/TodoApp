25bfb131fc7b1c881762c079d8bfc216bd197826
jasontermine <jason.termine@wiss-edu.ch>
2023-12-16 15:13:08 +0100

    removed unnecessary imports

--- backend/src/main/java/com/wiss/m223/model/Todo.java
+++ backend/src/main/java/com/wiss/m223/model/Todo.java
@@ -1,18 +1,10 @@
 package com.wiss.m223.model;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import com.wiss.m223.model.Status.EStatus;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
