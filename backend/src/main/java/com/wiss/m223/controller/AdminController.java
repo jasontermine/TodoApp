0c58ff2598340443f9faee27e15a2dc9ca648aba
jasontermine <jason.termine@wiss-edu.ch>
2023-12-18 09:07:32 +0100

    Added a Delete Endpoint for deleting Todos by ID

--- backend/src/main/java/com/wiss/m223/controller/AdminController.java
+++ backend/src/main/java/com/wiss/m223/controller/AdminController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,17 @@ public class AdminController {
 
 		Todo _todo = todoRepository.save(todo);
 		return new ResponseEntity<>(_todo, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/todos/delete/{id}")
+	public ResponseEntity<HttpStatus> deleteTodoById(@PathVariable("id") long id) {
+		try {
+			todoRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
 	}
 	
 }
