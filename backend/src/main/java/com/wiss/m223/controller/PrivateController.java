1490b023dce91042b83be42d453f44dbee743ccd
jasontermine <jason.termine@wiss-edu.ch>
2023-12-18 10:43:59 +0100

    updated getTodo Endpoint to private url

--- backend/src/main/java/com/wiss/m223/controller/PrivateController.java
+++ backend/src/main/java/com/wiss/m223/controller/PrivateController.java
@@ -1,15 +1,34 @@
 package com.wiss.m223.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wiss.m223.model.Todo;
+import com.wiss.m223.repository.TodoRepository;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/private")
 public class PrivateController {
 	
-	@GetMapping("/private")
-	public ResponseEntity<String> getGreeting() {
-		return ResponseEntity.ok("Hello Private World");
-	} 
+	@Autowired
+	TodoRepository todoRepository;
+
+	@GetMapping("")
+	public ResponseEntity<String> greeting() {
+		return new ResponseEntity<>("Hallo, du bist im Private", HttpStatus.OK);
+	}
+	@GetMapping("/todos")
+	public ResponseEntity<List<Todo>> getTodo() {
+		List<Todo> todos = todoRepository.findAll();
+		return new ResponseEntity<>(todos, HttpStatus.OK);
+	}
 	
 }
