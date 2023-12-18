46deedd5500921e2330c02bf8d86c960ed39fc9e
jasontermine <jason.termine@wiss-edu.ch>
2023-12-18 13:37:45 +0100

    updated aesthetics for Login

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  MDBValidation,
+  MDBValidationItem,
+  MDBInput,
+  MDBInputGroup,
+  MDBBtn,
+  MDBCheckbox,
+} from "mdb-react-ui-kit";
 import AuthService from "../functions/auth.service";
 
 export default function Login() {
@@ -21,12 +29,60 @@ export default function Login() {
   };
 
   // for Username
-  const onChangeUsername = (e) => { setUsername(e.target.value); };
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
 
   // for password
-  const onChangePassword = (e) => { setPassword(e.target.value); };
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
   return (
+    <>
+    <h1 style={{ top: "0", position: "absolute", marginLeft: "-6rem"}} className="start-50">Login</h1>
+      <MDBValidation
+        onSubmit={handleSubmit}
+        className="w-75 h-75 position-absolute top-50 start-50 translate-middle"
+      >
+        <div style={{ marginLeft: "12rem" }}>
+          <MDBValidationItem className="col-md-10 mb-4">
+            <MDBInput
+              value={username}
+              name="fname"
+              onChange={onChangeUsername}
+              id="validationCustom01"
+              required
+              label="Username"
+            />
+          </MDBValidationItem>
+          <MDBValidationItem className="col-md-10 mb-4">
+            <MDBInput
+              value={password}
+              name="lname"
+              onChange={onChangePassword}
+              id="validationCustom02"
+              required
+              label="Password"
+            />
+          </MDBValidationItem>
+          <div
+            className="position-absolute start-50"
+            style={{ marginLeft: "-11rem" }}
+          >
+            <MDBBtn className="" type="submit" onClick={handleSubmit}>
+              Submit form
+            </MDBBtn>
+            <MDBBtn className="ms-5" type="reset">
+              Reset form
+            </MDBBtn>
+          </div>
+        </div>
+      </MDBValidation>
+    </>
+  );
+
+  /* return (
     <form onSubmit={handleSubmit} method="post">
       <label>
         Username:
@@ -50,5 +106,5 @@ export default function Login() {
       <br />
       <button type="submit">Login</button>
     </form>
-  );
+  ); */
 }
