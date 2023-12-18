31f121555de02b6a1d16097dd246421fdd5f1798
jasontermine <jason.termine@wiss-edu.ch>
2023-12-18 13:37:18 +0100

    reworked aesthetics and functionality to rendder Todo list as 'MDBcards'

--- frontend/src/pages/private.jsx
+++ frontend/src/pages/private.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import { getPrivateContent } from "../functions/getContent";
+import { MDBCard, MDBCardBody, MDBCardText, MDBCardTitle } from "mdb-react-ui-kit";
 
 export async function loader() {
   const data = await getPrivateContent();
@@ -11,9 +12,23 @@ export default function Private() {
   const { data } = useLoaderData();
 
   return (
-    <>
-      <h1>Privat!</h1>
-      <p>Hier sind die Serverdaten: {data}</p>
+    <>  
+    <div className="position-absolute" style={{marginTop:"-25rem", marginLeft: "34rem"}}>
+      <h1 className="mb-4 ms-5">Hier sind alle Todos:</h1>
+      <ul>
+        {data.map((item) => (
+          <MDBCard key={item.id} className="mb-5 shadow-5-strong">
+          <MDBCardBody>
+            <MDBCardTitle>{item.name} - Issue #{item.id}</MDBCardTitle>
+            <MDBCardText>
+              <strong>Nachricht: </strong>{ item.message }<br/>
+              <strong>Status: </strong> { item.status.name == "STATUS_OFFEN" ? "Offen" : item.status.name == "STATUS_IN_BEARBEITUNG" ? "In Bearbeitung" : "Abgeschlossen" }
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+        ))}
+      </ul>
+    </div>
     </>
   );
 }
