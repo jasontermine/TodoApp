dc5e423a1b2ec628e22b9eb273901dcfde2c52c1
jasontermine <jason.termine@wiss-edu.ch>
2023-12-19 19:19:56 +0100

    easter egg ;)

--- backend/src/main/java/com/wiss/m223/controller/PublicController.java
+++ backend/src/main/java/com/wiss/m223/controller/PublicController.java
@@ -1,7 +1,5 @@
 package com.wiss.m223.controller;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +13,8 @@ public class PublicController {
 	 * @return ResponseEntity, das eine Liste von Zeichenketten enthält, die Begrüssungen repräsentieren.
 	 */
 	@GetMapping("/public")
-	public ResponseEntity<List<String>> getGreeting() {
-		return ResponseEntity.ok(List.of("Chrigu zipfu", "Michi zipfu", "Phippu zipfu"));
+	public ResponseEntity<String> getGreeting() {
+		return ResponseEntity.ok("Hi Sven :)");
 	} 
 	
 }
