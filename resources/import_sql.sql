e9fe228dc02ab48ca81b76ec3c942fd2be8c1c4e
jasontermine <jason.termine@wiss-edu.ch>
2023-12-21 18:38:02 +0100

    updated sql import script

--- resources/import_sql.sql
+++ resources/import_sql.sql
@@ -1,11 +1,3 @@
-CREATE DATABASE IF NOT EXISTS todo;
-
-USE todo;
-
-CREATE USER 'todo_user'@'%' IDENTIFIED BY 'todo_user';
-
-GRANT ALL PRIVILEGES ON todo.* TO 'todo_user'@'%';
-
 -- Füge diese Daten in die Datenbank ein NACHDEM du die Applikation zum ersten mal gestartet hast
 INSERT INTO role (id, name) VALUES (1, 'ROLE_USER');
 INSERT INTO role (id, name) VALUES (2, 'ROLE_ADMIN');
